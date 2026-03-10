06b195b6cfbcc6565b577d98c126ad55aa441243
chenbincai@four.four-faith.com <982810642@qq.com>
2026-03-10 19:50:40 +0800

    提取参数

--- public/config.js
+++ public/config.js
@@ -1,8 +1,9 @@
 window.config = {
     // Google Satellite (s=satellite only, y=hybrid with labels)
-    mapUrl: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
+    mapUrl: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
     // Google Satellite (s=satellite only, y=hybrid with labels)
     //mapUrl: 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'
     // 高德地图
     //mapUrl: 'https://webst04.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}'
+    wsUrl:'ws://192.168.25.43:8087/api/ai/chat'
 };
